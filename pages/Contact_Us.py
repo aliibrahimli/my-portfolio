cbd1c22c0277dc770eca9b6c0c8c64f998ecfdff
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-11 17:23:18 +0400

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import re
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address: ", placeholder="Write your email address here...")
+    message = st.text_area("Your message here: ", placeholder="Write your message here...")
+    button = st.form_submit_button("Submit")
+
+    if button:
+        if not user_email or not message:
+            if not user_email:
+                st.error('Please write your email address before submitting.')
+            if not message:
+                st.error('Please write text before submitting.')
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", user_email):
+
+            st.error('Please enter a valid email address.')
+        else:
+            st.success('Message sent.')
+
