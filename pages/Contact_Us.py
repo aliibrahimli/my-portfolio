c7fe33035bece44bc985ab54c707e48a8a50a7a2
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-11 18:13:40 +0400

    Send email using the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,22 +1,25 @@
-import streamlit as st
 import re
+
+import streamlit as st
+import send_email
 
 st.header("Contact Me")
 
-with st.form(key="email_forms"):
+with st.form(key="email_formss"):
     user_email = st.text_input("Your email address: ", placeholder="Write your email address here...")
-    message = st.text_area("Your message here: ", placeholder="Write your message here...")
-    button = st.form_submit_button("Submit")
+    message1 = st.text_area("Your message here: ", placeholder="Write your message here...")
+    message1 = message1 + "\n" + user_email
 
+
+    button = st.form_submit_button("Submit")
     if button:
-        if not user_email or not message:
+        if not user_email or not message1:
             if not user_email:
                 st.error('Please write your email address before submitting.')
-            if not message:
+            if not message1:
                 st.error('Please write text before submitting.')
         elif not re.match(r"[^@]+@[^@]+\.[^@]+", user_email):
-
             st.error('Please enter a valid email address.')
         else:
             st.success('Message sent.')
-
+            send_email.send_email(message1)
--- send_email.py
+++ send_email.py
@@ -1,20 +1,22 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
+import streamlit
 
-receiver = "alibrahimli041@gmail.com"
-username = "alibrahimli041@gmail.com"
-password = "pckxbssmffhdbebb"
 
-context = ssl.create_default_context()
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    receiver = "alibrahimli041@gmail.com"
+    username = "alibrahimli041@gmail.com"
+    password = "pckxbssmffhdbebb"
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Hi! 
-How are you?
-Bye 
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+
+
+
