787cedc4d743538cd64c819f9163ff9ffce662da
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-12 20:01:41 +0400

    Email password encrypted

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 import streamlit
 
@@ -9,7 +10,7 @@ def send_email(message):
     port = 465
     receiver = "alibrahimli041@gmail.com"
     username = "alibrahimli041@gmail.com"
-    password = "pckxbssmffhdbebb"
+    password = os.getenv("PASSWORD")
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
